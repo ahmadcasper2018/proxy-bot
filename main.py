245fdac453b21c19d894b8b0478fe84b9c2f0ef8
ahmad-salman <ahmadsy88@gmail.com>
2023-06-30 17:38:43 +0300

    feat: setup proxies payment board and optimize

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters import Command, CommandStart
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils import executor
 
@@ -22,96 +22,96 @@ dp = Dispatcher(bot, storage=storage)
 
 # Define states for the conversation
 class ButtonState(StatesGroup):
-    BUTTON1 = State()
-    BUTTON2 = State()
-    BUTTON3 = State()
-    BUTTON4 = State()
-    BUTTON5 = State()
+    MAIN_MENU = State()
+    PAYMENT = State()
+    ACCOUNT = State()
+    SUPPORT = State()
+    CHARGE = State()
 
 
-# Handler for the /start command
-@dp.message_handler(Command("start"))
+# Button texts
+BUTTON_TEXTS = {
+    "main_menu": ["شراء بروكسي", "حسابي", "التواصل مع الدعم", "إضافة رصيد", "حول البوت"],
+    "payment_options": ["شراء بروكسي يومي SOCKS 5", "شراء بروكسي مؤقت SOCKS 5", "شراء مودم روتيت", "شراء روتيت يومي"],
+    "back_button": "رجوع ↪️"
+}
+
+
+# Handler for the /start command or when the bot is started
+@dp.message_handler(CommandStart())
 async def start_command(message: types.Message):
+    await show_main_menu(message)
+
+
+# Show the main menu
+async def show_main_menu(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.row("شراء بروكسي", "حسابي")
-    keyboard.row("التواصل مع الدعم", "إضافة رصيد")
-    keyboard.add("حول البوت")
+    buttons = BUTTON_TEXTS["main_menu"]
+    keyboard.add(*buttons)
+    await bot.send_message(message.chat.id, "اختر طلبك 👇🏻 :", reply_markup=keyboard)
+
+
+# Show the payment options menu
+async def show_payment_options(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = BUTTON_TEXTS["payment_options"]
+    for button_text in buttons:
+        keyboard.add(button_text)
+    keyboard.add(BUTTON_TEXTS["back_button"])
     await bot.send_message(message.chat.id, "اختر طلبك 👇🏻 :", reply_markup=keyboard)
 
 
 # Handlers for button selections
-@dp.message_handler(text=["شراء بروكسي"], state="*")
-async def button1_selected(message: types.Message, state: FSMContext):
-    await ButtonState.BUTTON1.set()
-    await message.reply("You selected Button 1. Write your reply message:")
+@dp.message_handler(text=BUTTON_TEXTS["main_menu"], state="*")
+async def main_menu_selected(message: types.Message, state: FSMContext):
+    await ButtonState.MAIN_MENU.set()
+    if message.text == "شراء بروكسي":
+        await ButtonState.PAYMENT.set()
+        await show_payment_options(message)
+    elif message.text == "حسابي":
+        await ButtonState.ACCOUNT.set()
+        await message.reply("You selected Button حسابي. Write your reply message:")
+    elif message.text == "التواصل مع الدعم":
+        await ButtonState.SUPPORT.set()
+        await bot.send_message(message.chat.id, "التواصل مع الدعم:\nيمكنك التواصل مع الدعم على الرابط التالي: https://t.me/Proxies_bot_support")
+    elif message.text == "إضافة رصيد":
+        await ButtonState.CHARGE.set()
+        await message.reply("You selected Button إضافة رصيد. Write your reply message:")
+    elif message.text == "حول البوت":
+        await ButtonState.MAIN_MENU.set()
+        channel_link = "رابط القناة:\nhttps://t.me/Proxies1Channel"
+        group_link = "رابط المجموعة:\nhttp://t.me/Proxies_Group_Chat"
+        support_link = "رابط مراسلة الدعم للاستفسار:\nhttp://t.me/Proxies_bot_support"
+        reply_message = f"{channel_link}\n\n{group_link}\n\n{support_link}"
+        await bot.send_message(message.chat.id, reply_message)
 
 
-@dp.message_handler(text=["حسابي"], state="*")
-async def button2_selected(message: types.Message, state: FSMContext):
-    await ButtonState.BUTTON2.set()
-    await message.reply("You selected Button 2. Write your reply message:")
-
-
-@dp.message_handler(text=["التواصل مع الدعم"], state="*")
-async def button3_selected(message: types.Message, state: FSMContext):
-    await ButtonState.BUTTON3.set()
-    await bot.send_message(message.chat.id,
-                           "التواصل مع الدعم:\nيمكنك التواصل مع الدعم على الرابط التالي: https://t.me/Proxies_bot_support")
-
-
-@dp.message_handler(text=["إضافة رصيد"], state="*")
-async def button4_selected(message: types.Message, state: FSMContext):
-    await ButtonState.BUTTON4.set()
-    await message.reply("You selected Button 4. Write your reply message:")
-
-
-@dp.message_handler(text=["حول البوت"], state="*")
-async def button5_selected(message: types.Message, state: FSMContext):
-    await ButtonState.BUTTON5.set()
-    channel_link = "رابط القناة:\nhttps://t.me/Proxies1Channel"
-    group_link = "رابط المجموعة:\nhttp://t.me/Proxies_Group_Chat"
-    support_link = "رابط مراسلة الدعم للاستفسار:\nhttp://t.me/Proxies_bot_support"
-    reply_message = f"{channel_link}\n\n{group_link}\n\n{support_link}"
-    await bot.send_message(message.chat.id, reply_message)
+@dp.message_handler(text=[BUTTON_TEXTS["back_button"]], state="*")
+async def cancel_selected(message: types.Message, state: FSMContext):
+    await state.finish()  # Clear the current state
+    await show_main_menu(message)
 
 
 # Handler for reply messages
-@dp.message_handler(state=ButtonState.BUTTON1)
-async def handle_button1_reply(message: types.Message, state: FSMContext):
+@dp.message_handler(state=ButtonState.ACCOUNT)
+async def handle_button_account_reply(message: types.Message, state: FSMContext):
     reply_message = message.text
     await state.finish()
-    await message.reply(f"You replied to Button 1: {reply_message}")
+    await message.reply(f"You replied to Button حسابي: {reply_message}")
 
 
-@dp.message_handler(state=ButtonState.BUTTON2)
-async def handle_button2_reply(message: types.Message, state: FSMContext):
+@dp.message_handler(state=ButtonState.SUPPORT)
+async def handle_button_support_reply(message: types.Message, state: FSMContext):
     reply_message = message.text
     await state.finish()
-    await message.reply(f"You replied to Button 2: {reply_message}")
+    await message.reply(f"You replied to Button الدعم: {reply_message}")
 
 
-@dp.message_handler(state=ButtonState.BUTTON3)
-async def handle_button3_reply(message: types.Message, state: FSMContext):
+@dp.message_handler(state=ButtonState.CHARGE)
+async def handle_button_charge_reply(message: types.Message, state: FSMContext):
     reply_message = message.text
     await state.finish()
-    await message.reply(f"You replied to Button 3: {reply_message}")
-
-
-@dp.message_handler(state=ButtonState.BUTTON4)
-async def handle_button4_reply(message: types.Message, state: FSMContext):
-    reply_message = message.text
-    await state.finish()
-    await message.reply(f"You replied to Button 4: {reply_message}")
-
-
-@dp.message_handler(state=ButtonState.BUTTON5)
-async def handle_button5_reply(message: types.Message, state: FSMContext):
-    await state.finish()
-    channel_link = "رابط القناة:\nhttps://t.me/Proxies1Channel"
-    group_link = "رابط المجموعة:\nhttp://t.me/Proxies_Group_Chat"
-    support_link = "رابط مراسلة الدعم للاستفسار:\nhttp://t.me/Proxies_bot_support"
-    reply_message = f"{channel_link}\n\n{group_link}\n\n{support_link}"
-    await bot.send_message(message.chat.id, reply_message)
+    await message.reply(f"You replied to Button إضافة رصيد: {reply_message}")
 
 
 if __name__ == "__main__":
