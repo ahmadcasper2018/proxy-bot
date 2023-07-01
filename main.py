531673df6c4d4cf7b2df44a06df9465155133760
ahmad-salman <ahmadsy88@gmail.com>
2023-07-01 17:54:21 +0300

    feat: create user wallet

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command, CommandStart
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils import executor
+import aiomysql
+import asyncio
 
 load_dotenv()
 
@@ -18,6 +20,19 @@ bot_token = os.getenv("BOT_TOKEN")
 bot = Bot(token=bot_token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_database = os.getenv("DB_DATABASE")
+
+
+async def create_pool():
+    return await aiomysql.create_pool(host=db_host, user=db_user, password=db_password, db=db_database)
+
+
+loop = asyncio.get_event_loop()
+pool = loop.run_until_complete(create_pool())
 
 
 # Define states for the conversation
@@ -31,7 +46,7 @@ class ButtonState(StatesGroup):
 
 # Button texts
 BUTTON_TEXTS = {
-    "main_menu": ["شراء بروكسي", "حسابي", "التواصل مع الدعم", "إضافة رصيد", "حول البوت"],
+    "main_menu": ["شراء بروكسي", "حسابي", "إضافة رصيد", "التواصل مع الدعم", "حول البوت"],
     "payment_options": ["شراء بروكسي يومي SOCKS 5", "شراء بروكسي مؤقت SOCKS 5", "شراء مودم روتيت", "شراء روتيت يومي"],
     "back_button": "رجوع ↪️"
 }
@@ -40,7 +55,29 @@ BUTTON_TEXTS = {
 # Handler for the /start command or when the bot is started
 @dp.message_handler(CommandStart())
 async def start_command(message: types.Message):
+    user_id = message.from_user.id
+    wallet_balance = await get_user_wallet(user_id)
+    if wallet_balance is None:
+        await create_user_wallet(user_id)
     await show_main_menu(message)
+
+
+async def get_user_wallet(user_id):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("SELECT balance FROM wallets WHERE user_id = %s", (user_id,))
+            result = await cur.fetchone()
+            if result:
+                return result[0]
+            else:
+                return None
+
+
+async def create_user_wallet(user_id):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("INSERT INTO wallets (user_id, balance) VALUES (%s, 0)", (user_id,))
+            await conn.commit()
 
 
 # Show the main menu
@@ -70,10 +107,15 @@ async def main_menu_selected(message: types.Message, state: FSMContext):
         await show_payment_options(message)
     elif message.text == "حسابي":
         await ButtonState.ACCOUNT.set()
-        await message.reply("You selected Button حسابي. Write your reply message:")
+        user_id = message.from_user.id
+        wallet_balance = await get_user_wallet(user_id)
+        await state.finish()
+        reply_message = f"رصيدي ل.س: {wallet_balance}\nرصيدي بروكسيات يومية: 0\nالايدي الخاص بي: {user_id}"
+        await bot.send_message(message.chat.id, reply_message)
     elif message.text == "التواصل مع الدعم":
         await ButtonState.SUPPORT.set()
-        await bot.send_message(message.chat.id, "التواصل مع الدعم:\nيمكنك التواصل مع الدعم على الرابط التالي: https://t.me/Proxies_bot_support")
+        await bot.send_message(message.chat.id,
+                               "التواصل مع الدعم:\nيمكنك التواصل مع الدعم على الرابط التالي: https://t.me/Proxies_bot_support")
     elif message.text == "إضافة رصيد":
         await ButtonState.CHARGE.set()
         await message.reply("You selected Button إضافة رصيد. Write your reply message:")
