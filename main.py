a4c127f7f87f7990925d0d29977d10bc6b343d18
ahmad-salman <ahmadsy88@gmail.com>
2023-07-06 09:45:49 +0300

    feat

--- main.py
+++ main.py
@@ -42,24 +42,16 @@ class ButtonState(StatesGroup):
     ACCOUNT = State()
     SUPPORT = State()
     CHARGE = State()
+    TEMP_SOCKS_MENU = State()
 
 
 # Button texts
 BUTTON_TEXTS = {
     "main_menu": ["شراء بروكسي", "حسابي", "إضافة رصيد", "التواصل مع الدعم", "حول البوت"],
     "payment_options": ["شراء بروكسي يومي SOCKS 5", "شراء بروكسي مؤقت SOCKS 5", "شراء مودم روتيت", "شراء روتيت يومي"],
-    "back_button": "رجوع ↪️"
+    "back_button": "رجوع ↪️",
+    "temp_socks_menu": ["شراء بروكسي بورت (9800)", "شراء باقة بروكسيات يومية", "اإمكانية إعادة حساب", "Button 4", "Button 5"]
 }
-
-
-# Handler for the /start command or when the bot is started
-@dp.message_handler(CommandStart())
-async def start_command(message: types.Message):
-    user_id = message.from_user.id
-    wallet_balance = await get_user_wallet(user_id)
-    if wallet_balance is None:
-        await create_user_wallet(user_id)
-    await show_main_menu(message)
 
 
 async def get_user_wallet(user_id):
@@ -76,7 +68,15 @@ async def get_user_wallet(user_id):
 async def create_user_wallet(user_id):
     async with pool.acquire() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("INSERT INTO wallets (user_id, balance) VALUES (%s, 0)", (user_id,))
+            # Check if the user already has a wallet
+            await cur.execute("SELECT balance FROM wallets WHERE user_id = %s", (user_id,))
+            result = await cur.fetchone()
+            if result:
+                # User already has a wallet, update the balance
+                return
+            else:
+                # User doesn't have a wallet, insert a new row
+                await cur.execute("INSERT INTO wallets (user_id, balance) VALUES (%s, 0)", (user_id,))
             await conn.commit()
 
 
@@ -92,6 +92,68 @@ async def show_main_menu(message: types.Message):
 async def show_payment_options(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = BUTTON_TEXTS["payment_options"]
+    for button_text in buttons:
+        keyboard.add(button_text)
+    keyboard.add(BUTTON_TEXTS["back_button"])
+    await bot.send_message(message.chat.id, "اختر طلبك 👇🏻 :", reply_markup=keyboard)
+
+
+# Show the temporary SOCKS menu
+
+
+# Handlers for button selections
+@dp.message_handler(text=BUTTON_TEXTS["main_menu"], state="*")
+async def main_menu_selected(message: types.Message, state: FSMContext):
+    await ButtonState.MAIN_MENU.set()
+    if message.text == "شراء بروكسي":
+        await ButtonState.PAYMENT.set()
+        await show_payment_options(message)
+    elif message.text == "حسابي":
+        await ButtonState.ACCOUNT.set()
+        user_id = message.from_user.id
+        wallet_balance = await get_user_wallet(user_id)
+        await state.finish()
+        reply_message = f"رصيدي ل.س: {wallet_balance}\nرصيدي بروكسيات يومية: 0\nالايدي الخاص بي: {user_id}"
+        await bot.send_message(message.chat.id, reply_message)
+    elif message.text == "التواصل مع الدعم":
+        await ButtonState.SUPPORT.set()
+        await bot.send_message(message.chat.id,
+                               "التواصل مع الدعم:\nيمكنك التواصل مع الدعم على الرابط التالي: https://t.me/Proxies_bot_support")
+    elif message.text == "إضافة رصيد":
+        await ButtonState.CHARGE.set()
+        await message.reply("You selected Button إضافة رصيد. Write your reply message:")
+    elif message.text == "حول البوت":
+        await ButtonState.MAIN_MENU.set()
+        channel_link = "رابط القناة:\nhttps://t.me/Proxies1Channel"
+        group_link = "رابط المجموعة:\nhttp://t.me/Proxies_Group_Chat"
+        support_link = "رابط مراسلة الدعم للاستفسار:\nhttp://t.me/Proxies_bot_support"
+        reply_message = f"{channel_link}\n\n{group_link}\n\n{support_link}"
+        await bot.send_message(message.chat.id, reply_message)
+
+
+@dp.message_handler(text=BUTTON_TEXTS["payment_options"], state=ButtonState.PAYMENT)
+async def payment_option_selected(message: types.Message, state: FSMContext):
+    if message.text == "شراء بروكسي يومي SOCKS 5":
+        await ButtonState.TEMP_SOCKS_MENU.set()
+        await show_temp_socks_menu(message)
+
+
+@dp.message_handler(text=[BUTTON_TEXTS["back_button"]], state=ButtonState.TEMP_SOCKS_MENU)
+async def temp_socks_menu_back_selected(message: types.Message, state: FSMContext):
+    await state.finish()  # Clear the current state
+    await show_payment_options(message)
+
+
+@dp.message_handler(text=[BUTTON_TEXTS["back_button"]], state=ButtonState.PAYMENT)
+async def payment_options_back_selected(message: types.Message, state: FSMContext):
+    await state.finish()  # Clear the current state
+    await show_main_menu(message)
+
+
+# Show the temporary SOCKS menu
+async def show_temp_socks_menu(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = BUTTON_TEXTS["temp_socks_menu"]
     for button_text in buttons:
         keyboard.add(button_text)
     keyboard.add(BUTTON_TEXTS["back_button"])
@@ -128,34 +190,38 @@ async def main_menu_selected(message: types.Message, state: FSMContext):
         await bot.send_message(message.chat.id, reply_message)
 
 
-@dp.message_handler(text=[BUTTON_TEXTS["back_button"]], state="*")
-async def cancel_selected(message: types.Message, state: FSMContext):
+@dp.message_handler(text=BUTTON_TEXTS["payment_options"], state=ButtonState.PAYMENT)
+async def payment_option_selected(message: types.Message, state: FSMContext):
+    if message.text == "شراء بروكسي يومي SOCKS 5":
+        await ButtonState.TEMP_SOCKS_MENU.set()
+        await show_temp_socks_menu(message)
+
+
+@dp.message_handler(text=[BUTTON_TEXTS["back_button"]], state=ButtonState.TEMP_SOCKS_MENU)
+async def temp_socks_menu_back_selected(message: types.Message, state: FSMContext):
+    await state.finish()  # Clear the current state
+    await show_payment_options(message)
+
+
+@dp.message_handler(text=[BUTTON_TEXTS["back_button"]], state=ButtonState.PAYMENT)
+async def payment_options_back_selected(message: types.Message, state: FSMContext):
     await state.finish()  # Clear the current state
     await show_main_menu(message)
 
 
-# Handler for reply messages
-@dp.message_handler(state=ButtonState.ACCOUNT)
-async def handle_button_account_reply(message: types.Message, state: FSMContext):
-    reply_message = message.text
-    await state.finish()
-    await message.reply(f"You replied to Button حسابي: {reply_message}")
-
-
-@dp.message_handler(state=ButtonState.SUPPORT)
-async def handle_button_support_reply(message: types.Message, state: FSMContext):
-    reply_message = message.text
-    await state.finish()
-    await message.reply(f"You replied to Button الدعم: {reply_message}")
-
-
 @dp.message_handler(state=ButtonState.CHARGE)
-async def handle_button_charge_reply(message: types.Message, state: FSMContext):
-    reply_message = message.text
-    await state.finish()
-    await message.reply(f"You replied to Button إضافة رصيد: {reply_message}")
+async def charge_balance(message: types.Message, state: FSMContext):
+    # Process the user's reply message here
+    await state.finish()  # Clear the current state
+    await show_main_menu(message)
+
+
+# Start command handler
+@dp.message_handler(CommandStart())
+async def start(message: types.Message):
+    await create_user_wallet(message.from_user.id)
+    await show_main_menu(message)
 
 
 if __name__ == "__main__":
-    # Start the bot
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
